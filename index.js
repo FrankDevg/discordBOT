dd1fe9f42ddc6ddc2e37e2840ca0374963bd4147
Andrés Ruiz <franklin.ruiz@epn.edu.ec>
2021-04-11 23:22:42 -0500

    Conexion DISCORD API

---
+++ index.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready',()=>{
+    console.log('Bot is ready! IS GOOD')
+});
+
+client.login('ODMxMDE0NjU3NDY3NjEzMjA1.YHPEtQ.L0u98lIwHPbxdrPEuSJZ8gs-cRk');
