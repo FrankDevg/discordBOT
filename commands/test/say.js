2e2d3ab511c07b305709e2633787d97494861de5
themailof <andyruizz_db@hotmail.com>
2021-05-05 19:46:05 -0500

    Verificacion por tipo de argumentos!

--- commands/test/say.js
+++ commands/test/say.js
@@ -23,6 +23,7 @@ module.exports = class SayCommand extends commands.Command {
         })
     }
     execute(msg,args) {
+        console.log('/////' +args);
         msg.channel.send(args.join(' '));
     }
 
