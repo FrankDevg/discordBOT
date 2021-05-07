5367d021f4236819f51e92d29b3db4aba8c32661
themailof <andyruizz_db@hotmail.com>
2021-05-06 20:10:08 -0500

    funcion meme random

--- services/meme-service.js
+++ services/meme-service.js
@@ -1,17 +1,18 @@
   'use strict'
 const { Client, MessageEmbed, MessageAttachment } = require('discord.js');
+const lang = require('../util.js').getLanguage();
 
   const memeSchema = require('../services/models/meme-schema');
   module.exports ={
      saveMeme:async function(msg){
         await memeSchema.findOneAndUpdate( 
             {
-               _id:1
+               _id:10
            },
            {
-               _id:1,
-               keywords:"Ya estoy art@",
-               memeURL:"831187746986000424/831187760425074708/images.png"
+               _id:10,
+               keywords:"Dorime",
+               memeURL:"831187746986000424/831193209329680434/dorime11576397150.png"
            },
            {
                upsert: true,
@@ -29,14 +30,32 @@ const { Client, MessageEmbed, MessageAttachment } = require('discord.js');
            });
     },
     getMeme:async function(msg,id){
+        var  totalMemes;
+        if(id=='random') {
+            //total de documentos memes!
+            await memeSchema.countDocuments({}, function( err, count){
+                totalMemes=count;
+                console.log(count);
+
+            });
+
+            id =  Math.floor(Math.random() * totalMemes)+1;
+            console.log(id);
+        }
+
         await memeSchema.findOne( {
-            _id:1
+            _id:id
         }, function(err, result) {
             if (err) throw err;
-            console.log(result);
+            console.log("asdfasdfasdfasd   "+result);
             let avatarURL = (msg.author.avatarURL()) ? msg.author.avatarURL() : 'https://th.bing.com/th/id/OIP.QGjlnckx4xMewe5flHMPpgHaFC?pid=ImgDet&rs=1';
+            if(result!==null){
+                msg.channel.send(new MessageEmbed().setColor(0x02b9ba).setAuthor("" + msg.author.username, "" + avatarURL).setImage('https://cdn.discordapp.com/attachments/' + result.memeURL));
 
-            msg.channel.send(new MessageEmbed().setColor(0x02b9ba).setAuthor("" + msg.author.username, "" + avatarURL).setImage('https://cdn.discordapp.com/attachments/' + result.memeURL));
+            }else{
+                msg.channel.send(new MessageEmbed().setColor("RED").setDescription(lang.error.noFound.meme));
+
+            }
           });
     }
   }
